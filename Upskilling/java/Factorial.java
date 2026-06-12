d2de9e023410bae80b9ae98f80af9bf5684ebde0
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-12 09:45:03 +0530

    feat: ex11 factorial calculator

---
+++ Upskilling/java/Factorial.java
@@ -0,0 +1,20 @@
+package Upskilling.java;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int n = sc.nextInt();
+
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println(n + "! = " + result);
+        sc.close();
+    }
+}
