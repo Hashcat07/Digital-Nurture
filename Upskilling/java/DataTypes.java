48bf2a7e62948b75ef7e0afccbedcc152c53604f
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-12 09:18:49 +0530

    feat: ex06 declare and display primitives

---
+++ Upskilling/java/DataTypes.java
@@ -0,0 +1,17 @@
+package Upskilling.java;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int     age    = 25;
+        float   height = 5.9f;
+        double  salary = 75000.50;
+        char    grade  = 'A';
+        boolean active = true;
+
+        System.out.println("int:     " + age);
+        System.out.println("float:   " + height);
+        System.out.println("double:  " + salary);
+        System.out.println("char:    " + grade);
+        System.out.println("boolean: " + active);
+    }
+}
