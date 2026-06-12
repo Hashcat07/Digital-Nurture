fe850b4158ca8cfbf70569f6f3b2779f71cabb6a
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-12 09:53:17 +0530

    feat: ex14 calculate sum and average of array

---
+++ Upskilling/java/ArraySumAverage.java
@@ -0,0 +1,25 @@
+package Upskilling.java;
+
+import java.util.Scanner;
+
+public class ArraySumAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        for (int x : arr) sum += x;
+
+        System.out.println("Sum:     " + sum);
+        System.out.println("Average: " + (double) sum / n);
+        sc.close();
+    }
+}
