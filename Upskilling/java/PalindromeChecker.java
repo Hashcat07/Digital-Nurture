b036939eb33f37ac8f77f55495d6c669aabe0f86
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-12 09:57:47 +0530

    feat: ex16 palindrome checker

---
+++ Upskilling/java/PalindromeChecker.java
@@ -0,0 +1,24 @@
+package Upskilling.java;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine()
+                         .toLowerCase()
+                         .replaceAll("[^a-z0-9]", "");
+
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        if (input.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+
+        sc.close();
+    }
+}
