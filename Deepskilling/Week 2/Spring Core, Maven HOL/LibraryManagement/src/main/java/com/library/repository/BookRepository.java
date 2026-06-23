507042e07d8e1a8689ca68be6f5a444c5488b27f
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-23 18:55:59 +0530

    feat: add ex1 using classPathXmlApplicationContext

---
+++ Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public String getBookTitle(int id) {
+        return "Book Temp (id=" + id + ")";
+    }
+}
---
+++ Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+public class BookService {
+
+    public void run() {
+        System.out.println("BookService is up and running.");
+    }
+}
