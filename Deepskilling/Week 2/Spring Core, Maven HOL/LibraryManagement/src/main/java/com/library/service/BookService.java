f33425ad71c9e22ab9698b1565dd658a5b6e100a
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-23 19:26:44 +0530

    feat: add AOP concepts and used @Around annotation

--- Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -11,6 +11,11 @@ public class BookService {
     }
 
     public void printBook(int id){
+        try{
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         String title=bookRepository.getBookTitle(id);
         System.out.println("Fetched: "+title);
     }
