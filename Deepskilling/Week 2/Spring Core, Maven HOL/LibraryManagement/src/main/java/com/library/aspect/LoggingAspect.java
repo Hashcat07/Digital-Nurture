b52ab00bf57dc00934432a0a306353b01848a97d
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-23 20:04:50 +0530

    feat: use AOP @Before and @After

--- Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
+++ Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -1,20 +1,32 @@
 package com.library.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LoggingAspect {
 
-    @Around("execution(* com.library.service.*.*(..))")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
+//    @Around("execution(* com.library.service.*.*(..))")
+//    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+//        long start = System.currentTimeMillis();
+//
+//        Object result = joinPoint.proceed();
+//
+//        long elapsed = System.currentTimeMillis() - start;
+//        System.out.println("[AOP] " + joinPoint.getSignature() + " ran in " + elapsed + " ms");
+//        return result;
+//    }
+    @Before("execution(* com.library.service.*.*(..))")
+        public void beforeAdvice(JoinPoint joinPoint) {
+        System.out.println("[BEFORE] calling " + joinPoint.getSignature().getName());
+}
 
-        Object result = joinPoint.proceed();
-
-        long elapsed = System.currentTimeMillis() - start;
-        System.out.println("[AOP] " + joinPoint.getSignature() + " ran in " + elapsed + " ms");
-        return result;
+    @After("execution(* com.library.service.*.*(..))")
+    public void afterAdvice(JoinPoint joinPoint) {
+        System.out.println("[AFTER]  finished " + joinPoint.getSignature().getName());
     }
 }
