abf7abc344bef3e7472746e85d968a3b34f79844
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-23 19:39:50 +0530

    feat: add Annotations instead of <bean/>

--- Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
+++ Deepskilling/Week 2/Spring Core, Maven HOL/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -8,7 +8,7 @@ public class LibraryManagementApplication {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        BookService bookService = context.getBean("bookService", BookService.class);
+        BookService bookService = context.getBean(BookService.class);
 //        bookService.run();
         bookService.printBook(101);
         context.close();
