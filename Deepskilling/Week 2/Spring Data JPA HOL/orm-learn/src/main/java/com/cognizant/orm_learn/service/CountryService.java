ad3e09666bad1b6a0e83fc62b89fc829053956f6
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-24 21:39:14 +0530

    feat: complete ex_1 hands on 6

--- Deepskilling/Week 2/Spring Data JPA HOL/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
+++ Deepskilling/Week 2/Spring Data JPA HOL/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -5,6 +5,8 @@ import com.cognizant.orm_learn.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
 
 import java.util.List;
 
@@ -17,5 +19,17 @@ public class CountryService {
     @Transactional
     public List<Country> getAllCountries() {
         return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+
+        Optional<Country> country = countryRepository.findById(countryCode);
+
+        if(country.isEmpty()) {
+            throw new CountryNotFoundException("Country Not Found");
+        }
+
+        return country.get();
     }
 }
