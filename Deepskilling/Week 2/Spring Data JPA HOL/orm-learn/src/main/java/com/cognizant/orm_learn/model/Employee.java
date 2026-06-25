5b1a124e9e08cb553966ba2d7279e5f65e2caa10
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-25 19:12:27 +0530

    feat: commpleted ex_2 hand on 3

---
+++ Deepskilling/Week 2/Spring Data JPA HOL/orm-learn/src/main/java/com/cognizant/orm_learn/model/Employee.java
@@ -0,0 +1,29 @@
+package com.cognizant.orm_learn.model;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "em_id")
+    private int id;
+
+    @Column(name = "em_name")
+    private String name;
+
+    @Column(name = "em_salary")
+    private double salary;
+
+    @Column(name = "em_permanent")
+    private boolean permanent;
+
+    @Column(name = "em_date_of_birth")
+    private LocalDate dateOfBirth;
+
+}
