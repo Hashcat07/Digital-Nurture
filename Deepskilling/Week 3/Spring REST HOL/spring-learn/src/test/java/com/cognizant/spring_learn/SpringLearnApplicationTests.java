0188e13e484381fe6cf85e975ac889309ea4836a
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-30 18:51:32 +0530

    feat: complete ex_2 hands on 6

--- Deepskilling/Week 3/Spring REST HOL/spring-learn/src/test/java/com/cognizant/spring_learn/SpringLearnApplicationTests.java
+++ Deepskilling/Week 3/Spring REST HOL/spring-learn/src/test/java/com/cognizant/spring_learn/SpringLearnApplicationTests.java
@@ -1,13 +1,45 @@
 package com.cognizant.spring_learn;
 
+import com.cognizant.spring_learn.controller.CountryController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringLearnApplicationTests {
+
+	@Autowired
+	private CountryController countryController;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(countryController);
+	}
+
+	@Test
+	public void testGetCountry() throws Exception{
+		ResultActions actions= mockMvc.perform(get("/country"));
+
+		actions.andExpect(status().isOk());
+		actions.andExpect(jsonPath("$.code").exists());
+		actions.andExpect(jsonPath("$.code").value("IN"));
+		actions.andExpect(jsonPath("$.name").exists());
+		actions.andExpect(jsonPath("$.name").value("India"));
+
+
+
 	}
 
 }
