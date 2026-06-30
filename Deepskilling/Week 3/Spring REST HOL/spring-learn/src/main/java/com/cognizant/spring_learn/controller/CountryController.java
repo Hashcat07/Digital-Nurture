5f3a9eaa475c315675667f4858448e051a0c754a
RAY <166890725+Hashcat07@users.noreply.github.com>
2026-06-30 18:32:35 +0530

    feat: complete ex_2 hands on 3

--- Deepskilling/Week 3/Spring REST HOL/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ Deepskilling/Week 3/Spring REST HOL/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -5,8 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class CountryController {
@@ -22,5 +25,16 @@ public class CountryController {
 
         LOGGER.info("END");
         return country;
+    }
+
+    @GetMapping("/countries")
+    public ArrayList<Country> getAllCountries() {
+
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        ArrayList<Country> countries = context.getBean("countryList", ArrayList.class);
+        LOGGER.info("END");
+
+        return countries;
     }
 }
